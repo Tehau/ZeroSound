bfa8160b9d7f7e1db9489265286d43727ef2d691
kamek-pf <b.kamek@gmail.com>
2014-09-03 21:04:19 -1000

    main activity receives and displays commands

--- src/pf/kamek/zer0sound/activities/MainScreen.java
+++ src/pf/kamek/zer0sound/activities/MainScreen.java
@@ -4,22 +4,55 @@ import pf.kamek.zer0sound.R;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainScreen extends Activity
 {
 
         private AudioManager audioManager;
+        private TextView lastCommand;
+        private Bundle extras;
 
         @Override
         public void onCreate(Bundle savedInstanceState)
         {
                 super.onCreate(savedInstanceState);
+                Log.d("zer0Sound", "[MainScreen] :  onCreate() called");
                 setContentView(R.layout.main_screen);
+
                 audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+                lastCommand = (TextView) this.findViewById(R.id.last_command);
+                lastCommand.addTextChangedListener(new TextWatcher() {
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                                // TODO Auto-generated method stub
+
+                        }
+
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                                // TODO Auto-generated method stub
+
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                                Log.d("zer0Sound", "[MainScreen] : last command changed");
+                        } 
+
+                });
         }
+
+
 
         public void muteButtonClicked(View v)
         {
@@ -35,5 +68,26 @@ public class MainScreen extends Activity
                                 AudioManager.STREAM_MUSIC, 
                                 audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) / 2,
                                 AudioManager.FLAG_PLAY_SOUND);
+        }
+
+        protected void onResume() {
+                // TODO Auto-generated method stub
+                super.onResume();
+                Log.d("zer0Sound", "[MainScreen] :  onResume() called");
+                extras = this.getIntent().getExtras(); 
+                if (extras != null)
+                {
+                        Log.d("zer0Sound", "[MainScreen] : intent received : " + extras.getString("command"));
+                        if(extras.getString("command") != "[]")
+                                lastCommand.setText(extras.getString("command"));
+                }
+        }
+
+        @Override
+        protected void onNewIntent(Intent intent) {
+                // TODO Auto-generated method stub
+                super.onNewIntent(intent);
+                this.setIntent(intent);
+                Log.d("zer0Sound", "[MainScreen] :  onNewIntent() called");
         }
 }
--- src/pf/kamek/zer0sound/services/VocalService.java
+++ src/pf/kamek/zer0sound/services/VocalService.java
@@ -1,18 +1,28 @@
 package pf.kamek.zer0sound.services;
 
+import pf.kamek.zer0sound.activities.MainScreen;
+
 import android.accessibilityservice.AccessibilityService;
+import android.content.Intent;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
 public class VocalService extends AccessibilityService {
+
         @Override
         public void onAccessibilityEvent(AccessibilityEvent event) {
-                Log.d("zer0Sound", "Event : " + event.getText().toString());
+                String command = event.getText().toString();
+                Log.d("zer0Sound", "[VocalService] : " + command);
+                Intent intent = new Intent(this.getApplicationContext(), MainScreen.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                intent.putExtra("command", command); 
+                this.getApplicationContext().startActivity(intent);
         }
 
         @Override
         protected void onServiceConnected() {
-               super.onServiceConnected();
+                super.onServiceConnected();
         }
 
         @Override
